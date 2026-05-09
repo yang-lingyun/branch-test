7e90c3990283eef3970e1865aee57c17868eecd1
yangly <coderyun@foxmail.com>
2026-05-09 23:38:13 +0800

    V3.0.0

--- gxzl-infra-api/src/main/java/com/sy/cloud/infra/enums/ApiConstants.java
+++ gxzl-infra-api/src/main/java/com/sy/cloud/infra/enums/ApiConstants.java
@@ -8,25 +8,31 @@ import com.sy.cloud.framework.common.enums.RpcConstants;
  * @author 三壹科技
  */
 public class ApiConstants {
-
-    /**
-     * 服务名aaaaa
-     * <p>sasdasd
-     * 注意，需要保证和 spring.application.name 保持一致
-     */
-    public staticaa final String PREFIX = RpcConstants.RPC_API_PREFIX + "/infra";
-
-    public static final String VERSION = "1.0.0";
-aaa
-}dasdadasa
-aaa
-     * 服务名dasdasdasdasdasdasda
-     * <p>dasdasPREFIX = astring PREFIX = RpcConstants.RPC_API_PREFIX + "/infra";
-    public static finalas
-    public static finaldasdas
-    public static finaldasdas
-    public static finaldasdas
-    public static finaldasdas
-    public static finaldasdas
-    public static finaldasdas
-    public static finaldasdas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
+asdasasdasdasdasasdasas
