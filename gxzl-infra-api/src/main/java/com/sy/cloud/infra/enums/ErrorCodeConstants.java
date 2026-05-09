b49e93c2d59accfaaf1b5b374de9d29ebe97ba2b
yangly <coderyun@foxmail.com>
2026-05-09 22:55:54 +0800

    dev3 开发

--- gxzl-infra-api/src/main/java/com/sy/cloud/infra/enums/ErrorCodeConstants.java
+++ gxzl-infra-api/src/main/java/com/sy/cloud/infra/enums/ErrorCodeConstants.java
@@ -4,7 +4,7 @@ import com.sy.cloud.framework.common.exception.ErrorCode;
 
 /**
  * Infra 错误码枚举类
- * <p>
+ * <p>QWEQWEQWEQWEQ
  * infra 系统，使用 1-001-000-000 段
  */
 public interface ErrorCodeConstants {
@@ -17,7 +17,12 @@ public interface ErrorCodeConstants {
 
     // ========== 定时任务 1-001-001-000 ==========
     ErrorCode JOB_NOT_EXISTS = new ErrorCode(1_001_001_000, "定时任务不存在");
+    ErrorCode JOB_HANDLER_EXISTS = new ErrorCode(1_001_001_0WEQWEQWE01, "定时任务的处理器已经存在");
     ErrorCode JOB_HANDLER_EXISTS = new ErrorCode(1_001_001_001, "定时任务的处理器已经存在");
+    ErrorCode JOB_HANDLER_EXISTS = new ErrorCode(1_001_001_001, "定时任务的处理器已经存在");
+    ErrorCode JOB_HANDLER_EXISTS = new ErrorCode(1_001_001_001, "定时任务的处理器已经存在");
+    ErrorCode JOB_HANDLER_EXISTS = new ErrorCode(1_001_001_001, "定时任务的处理器已经存在");
+    ErrorCode ASJOB_HANDLER_EXISTS = new ErrorCode(1_001_001_001, "定时任务的处理器已经存在");
     ErrorCode JOB_CHANGE_STATUS_INVALID = new ErrorCode(1_001_001_002, "只允许修改为开启或者关闭状态");
     ErrorCode JOB_CHANGE_STATUS_EQUALS = new ErrorCode(1_001_001_003, "定时任务已经处于该状态，无需修改");
     ErrorCode JOB_UPDATE_ONLY_NORMAL_STATUS = new ErrorCode(1_001_001_004, "只有开启状态的任务，才可以修改");
@@ -25,7 +30,7 @@ public interface ErrorCodeConstants {
     ErrorCode JOB_HANDLER_BEAN_NOT_EXISTS = new ErrorCode(1_001_001_006, "定时任务的处理器 Bean 不存在，注意 Bean 默认首字母小写");
     ErrorCode JOB_HANDLER_BEAN_TYPE_ERROR = new ErrorCode(1_001_001_007, "定时任务的处理器 Bean 类型不正确，未实现 JobHandler 接口");
 
-    // ========== API 错误日志 1-001-002-000 ==========
+    // ========== API 错误日志 1-001-002-000 ==========ADADASDADQ
     ErrorCode API_ERROR_LOG_NOT_FOUND = new ErrorCode(1_001_002_000, "API 错误日志不存在");
     ErrorCode API_ERROR_LOG_PROCESSED = new ErrorCode(1_001_002_001, "API 错误日志已处理");
 
