0d049ed8585b58e78b7c67f2d91efd28ed99e88a
yangly <coderyun@foxmail.com>
2026-05-09 23:36:07 +0800

    dev5

--- gxzl-infra-api/src/main/java/com/sy/cloud/infra/enums/ApiConstants.java
+++ gxzl-infra-api/src/main/java/com/sy/cloud/infra/enums/ApiConstants.java
@@ -10,12 +10,13 @@ import com.sy.cloud.framework.common.enums.RpcConstants;
 public class ApiConstants {
 
     /**
-     * 服务名
-     * <p>
+     * 服务名aaaaa
+     * <p>sasdasd
      * 注意，需要保证和 spring.application.name 保持一致
      */
-    public static final String PREFIX = RpcConstants.RPC_API_PREFIX + "/infra";
+    public staticaa final String PREFIX = RpcConstants.RPC_API_PREFIX + "/infra";
 
     public static final String VERSION = "1.0.0";
-
+aaa
 }dasdadasa
+aaa
